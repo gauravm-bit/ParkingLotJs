d5ea8f9fd143c72d6d83e0f2b5bdb4f47e552524
gauravm-bit <gauravwrk01@gmail.com>
2020-04-26 17:46:46 +0530

    ADDED: testcase for driver to park the car

--- app/ParkingLot.js
+++ app/ParkingLot.js
@@ -1,1 +1,11 @@
-console.log("welcome");
+class parkingLot{
+    constructor(){
+        this.Lot = [];
+    }
+
+    doParking = (car) => {
+        this.Lot.push(car)
+        return true;
+    }
+}
+module.exports = new parkingLot
--- test/ParkingLotTest.js
+++ test/ParkingLotTest.js
@@ -1,1 +1,13 @@
- 
+const assert = require('chai').assert
+const parkingLot = require('../app/ParkingLot.js')
+
+let car = { }
+describe(`Testing for Parking Lot service`, () => {
+
+//TC 1.1 let the driver park so that he can board his flight
+it(`allow parking so the driver can board flight`, () => {
+    let result = parkingLot.doParking(car)
+    assert.isTrue(result)
+})
+
+})
